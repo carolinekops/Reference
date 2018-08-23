0f796e16fa2d9d6045d70b2b1e6fe1648787ab85
Caroline Kops <caroline.kops@uconn.edu>
2018-08-22 21:04:11 -0400

    hwk
    hwk

---
+++ src/StringFormatting.java
@@ -0,0 +1,9 @@
+
+public class StringFormatting {
+public static void main(String[] args) {
+	
+String output = String.format("%s %d", "Caroline", 22);
+System.out.println(output);
+System.out.printf("My name is: %s", "Caroline");
+}
+}
